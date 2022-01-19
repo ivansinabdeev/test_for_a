e04e3d12350fe41f0adc3ba5377be15078d1c14d
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2022-01-19 03:09:15 +0100

    add app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Yes from "./components/yes";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <Yes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/yes.js
@@ -0,0 +1,9 @@
+function Yes() {
+  return (
+    <div>
+      <p>hi, people</p>
+      <img src="./src/images/confetti/confetti.png" width="100%"></img>
+    </div>
+  );
+}
+export default Yes;
