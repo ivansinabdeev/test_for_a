8f8769c338b262593658889a7e49eb0a8f684a68
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2022-01-21 04:01:14 +0100

    add cup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Cup from "./components/Cup/Cup";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <Cup />
     </div>
   );
 }
---
+++ src/components/Cup/Cup.js
@@ -0,0 +1,13 @@
+import s from "./Cup.module.css";
+
+function Cup() {
+  return (
+    <div className={s.background}>
+      <div className={s.cup}>
+        <></>
+      </div>
+    </div>
+  );
+}
+
+export default Cup;
