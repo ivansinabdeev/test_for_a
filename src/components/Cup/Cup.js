2db53f29337ceab7e66a2c7127ec3f24abbde6fb
ivansinabdeev <75542138+ivansinabdeev@users.noreply.github.com>
2022-01-28 01:29:48 +0100

    update

--- src/components/Cup/Cup.js
+++ src/components/Cup/Cup.js
@@ -2,10 +2,13 @@ import s from "./Cup.module.css";
 
 function Cup() {
   return (
-    <div className={s.background}>
-      <div className={s.cup}></div>
-      <div className={s.confetti}></div>
-    </div>
+    <>
+      <div className={s.background}>
+        <div className={s.confetti}>
+          <div className={s.cup}></div>
+        </div>
+      </div>
+    </>
   );
 }
 
